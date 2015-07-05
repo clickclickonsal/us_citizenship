060a848a39b251ca36fc97704091154ccbdb59f9
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-07-05 14:38:37 -0400

    shoqng questions on main page now

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Category.destroy_all
-Category.create([
+cats = Category.create([
   { name: "American Govenment"}
 ])
 
 Type.destroy_all
-Type.create([
+types = Type.create([
   { name: "Principles of American Democracy"}
 ])
 
@@ -20,36 +20,36 @@ Question.destroy_all
 Question.create([
   { 
     question_text: "What is the supreme law of the land?", 
-    type_id: , 
-    catergory_id: , 
+    type_id: types.first.id, 
+    category_id: cats.first.id, 
     question_number: 1,
     answers: ["the Constitution"]
   },
   { 
     question_text: "What does the Constitution do?", 
-    type_id: , 
-    catergory_id: , 
+    type_id: types.first.id, 
+    category_id: cats.first.id, 
     question_number: 2,
     answers: ["sets up the government", "defines the government", " protects basic rights of Americans"]
   },
   { 
     question_text: "The idea of self-government is in the first three words of the Constitution. What are these words?", 
-    type_id: , 
-    catergory_id: , 
+    type_id: types.first.id, 
+    category_id: cats.first.id, 
     question_number: 3,
     answers: ["We the People"]
   },
   { 
     question_text: "What is an amendment?", 
-    type_id: , 
-    catergory_id: , 
+    type_id: types.first.id, 
+    category_id: cats.first.id, 
     question_number: 4,
     answers: ["a change (to the Constitution)", "an addition (to the Constitution)"]
   },
   { 
     question_text: "What do we call the first ten amendments to the Constitution?", 
-    type_id: , 
-    catergory_id: , 
+    type_id: types.first.id, 
+    category_id: cats.first.id, 
     question_number: 5,
     answers: ["the Bill of Rights"]
   },
