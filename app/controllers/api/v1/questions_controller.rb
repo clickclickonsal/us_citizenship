24d30b5a3a2a4bf4a9dc21f1ba716a9496de0f9c
clickclickonsal <s.hernandez5400@gmail.com>
2017-05-08 00:12:46 -0400

    return more information with response

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,10 +2,24 @@ class Api::V1::QuestionsController < ApplicationController
 
   before_action :set_question, only: [:show]
 
-  def index
-    @questions = Question.paginate(:page => params[:page], :per_page => params[:per_page])
+  attr_accessor :page, :per_page, :total_pages
 
-    render json: @questions, status: 200
+  def index
+    page = params[:page] || 1
+    per_page = params[:per_page] || Question.count
+
+    questions = Question.paginate(:page => page, :per_page => per_page)
+
+    total_pages = questions.total_entries / questions.per_page
+
+    # render json: questions, status: 200
+    render json: {
+      success: 200,
+      page: page,
+      per_page: per_page,
+      total_pages: total_pages,
+      results: questions
+    }, status: 200
   end
 
   def show
